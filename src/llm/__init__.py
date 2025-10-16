7a4a002cf1674098f20c6b0a1792b3b6a13b9da1
imtiazindia <128246603+imtiazindia@users.noreply.github.com>
2025-10-16 15:19:58 +0530

    Include OutputHandler in module exports
    Added OutputHandler to imports and __all__ list.

--- src/llm/__init__.py
+++ src/llm/__init__.py
@@ -6,3 +6,20 @@ from .models import *
 from .connection import OllamaConnectionManager
 from .auth import ModelManager
 from .manager import LLMManager
+
+from .models import *
+from .connection import OllamaConnectionManager
+from .auth import ModelManager
+from .manager import LLMManager
+from .output_handler import OutputHandler
+
+__all__ = [
+    "LLMConfig",
+    "LLMRequest", 
+    "LLMResponse",
+    "LLMError",
+    "OllamaConnectionManager",
+    "ModelManager",
+    "LLMManager", 
+    "OutputHandler"
+]
